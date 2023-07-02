899bd007df0c52d8520e157eb5af3d81d9e546be
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-07-02 17:41:59 -0500

    Update drivermod.lua

--- src/main/resources/assets/computercraft/lua/rom/programs/drivermod.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/drivermod.lua
@@ -1,1 +1,75 @@
-local function set(
+local args = {...}
+
+local function readLuaCode(filename)
+  local file = fs.open(filename, "r")
+  local lua_code_lines = {}
+
+  if file then
+    local line = file.readLine()
+    while line do
+      table.insert(lua_code_lines, line)
+      line = file.readLine()
+    end
+
+    file.close()
+  end
+
+  return lua_code_lines
+end
+
+local function writeLuaCode(filename, lua_code_lines)
+  local file = fs.open(filename, "w")
+
+  for i, line in ipairs(lua_code_lines) do
+    file.writeLine(line)
+  end
+
+  file.close()
+end
+
+local function appendDriverAssignment(filename, key, value)
+  local lua_code_lines = readLuaCode(filename)
+
+  -- Construct the assignment string
+  local assignment = string.format('driver["%s"] = "%s"', key, value)
+  table.insert(lua_code_lines, assignment)
+
+  writeLuaCode(filename, lua_code_lines)
+end
+
+local function getDriverValue(filename, key)
+  local lua_code_lines = readLuaCode(filename)
+
+  -- Find the assignment for the specified key
+  local pattern = string.format('driver\\["%s"\\]%s*=.-', key, '%s*')
+  for i, line in ipairs(lua_code_lines) do
+    if line:match(pattern) then
+      local value = line:match('"(.*)"')
+      print(value)
+      return
+    end
+  end
+
+  print("Key not found in the driver assignments.")
+end
+
+-- Main logic
+if #args < 3 then
+  print("Usage: drivers set [key] [value]")
+  print("       drivers get [key]")
+  print("\nFind examples of how to use the drivers in the 'examples' folder located in the folder 'rom'.")
+  return
+end
+
+local command = args[1]
+local key = args[2]
+local value = args[3]
+
+if command == "set" then
+  appendDriverAssignment("rom/driverbase.lua", key, value)
+  print("Driver assignment added successfully.")
+elseif command == "get" then
+  getDriverValue("rom/driverbase.lua", key)
+else
+  print("Invalid command.")
+end
